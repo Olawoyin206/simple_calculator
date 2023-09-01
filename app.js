39080136dafff96c341e2827dc570caee3ed90b0
thepublisher206 <olawoyin.olamide@tech-u.edu.ng>
2023-09-01 08:06:41 +0100

    seocnd commit

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-let screenn = '';
+// let screenn = '';
 
-function displayNumbers(value) {
-    screenn += value;
-    document.getElementById('display').value = screenn;
-}
+// function displayNumbers(value) {
+//     screenn += value;
+//     document.getElementById('display').value = screenn;
+// }
 
-function ClearWholeScreen() {
-    screenn = '';
-    document.getElementById('display').value = '';
-}
+// function ClearWholeScreen() {
+//     screenn = '';
+//     document.getElementById('display').value = '';
+// }
 
-function DeleteButton(value) {
-    if (value !== -1) {
-        screenn = screenn.slice(0, -1); // Remove the last character
-    } else {
-        displayNumbers(value);
-    }
-    document.getElementById('display').value = screenn;
-    screenn = '';
-}
+// function DeleteButton(value) {
+//     if (value !== -1) {
+//         screenn = screenn.slice(0, -1); // Remove the last character
+//     } else {
+//         displayNumbers(value);
+//     }
+//     document.getElementById('display').value = screenn;
+//     screenn = '';
+// }
 
-function Calculate() {
-        result = eval(screenn);
-        document.getElementById('display').value = result;
-        displayValue = result.toString();
-}
-function PiCalculation(){
-    pivalue = 3.14159265359 * screenn
-    document.getElementById('display').value = pivalue;
-    displayValue = pivalue.toString(); 
-}
+//     function Calculate() {
+//         result = eval(screenn);
+//         document.getElementById('display').value = result;
+// }
+
+// function PiCalculation(){
+//     pivalue = 3.14159265359 * screenn
+//     document.getElementById('display').value = pivalue;
+//     displayValue = pivalue.toString(); 
+// }
 
 
-// Currency Calculator
+// // Currency Calculator
